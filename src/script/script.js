d5226794faa693bad26bbf453ab54f0ac9e98073
beilunyang <786220806@qq.com>
2016-03-05 11:36:08 +0800

    导航条完成

--- src/script/script.js
+++ src/script/script.js
@@ -1,35 +1,40 @@
 $(function(){
-	var /*left_navbar = $('.navbar'),*/
-	   /* isleft = false,*/
-	    h = $('html'),
-	    search_input = $('.search-input'),
+	var isShow = false,
+	    /*h = $('html'),
+	    search_input = $('.search-input'),*/
 	    brand = $('.brand'),
-	    navbar = $('.navbar'),
-	    s_hints = $('.s-hints');
-	/*$('.more').on('click',function(){
-		if(isleft === true){
-			left_navbar.removeClass('open');
-			return isleft = false;
+	    navbar = $('.navbar');/*,
+	    s_hints = $('.s-hints');*/
+
+	$('.more').on('click',function(){
+		if(isShow){
+			navbar.animate({'height':0,'opacity':1},'fast');
+			return isShow = false;
 		}
-		left_navbar.addClass('open');
-		isleft = true;
-	});*/
-	$('.search').on('click',function(){
+
+		navbar.css({'display':'block'});
+		navbar.animate({'height':'100px','opacity':1},'fast');
+		isShow = true;
+	});
+
+	/*$('.search').on('click',function(){
 		search_input.addClass('open');
 		h.css('overflow','hidden');
 	});
 	$('.s-close').on('click',function(){
 		search_input.removeClass('open');
 		h.css('overflow','auto');
-	});
-	$('.input-group input').on('focus',function(){
+	});*/
+
+	/*$('.input-group input').on('focus',function(){
 		s_hints.css({opacity:1});
-	});
+	});*/
+
 	$(window).on('scroll',function(){
 		if($(this).scrollTop()>80){
 			brand.addClass('scroll');
 			navbar.css('display','block')
-				  .animate({'opacity':1},2000);
+				  .animate({'opacity':1},1000);
 			$(this).off('scroll');
 		}
 	});
